40c6eba4c9dad0ff66fe2fec2ea1bd7cf311627e
Hugh Ashley <ashley.hughm@gmail.com>
2023-01-18 20:01:49 -0500

    filters complete minus error handling

--- PrelimAssignment1/calendar_filter.c
+++ PrelimAssignment1/calendar_filter.c
@@ -59,24 +59,33 @@ int main(int argc, char *argv[]){
 				events[count].time.tm_isdst = 0;
 				strcpy(events[count].location, location);
 				for (int i = 0; i <= count; i++){
-					if (difftime(mktime(&events[count].time), mktime(&events[i].time)) == 0){
-						strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[i].time);
+					if (difftime(mktime(&events[count].time), mktime(&events[i].time)) == 0 && i != count){
+						strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[count].time);
 						printf("%s,%s\n", time, events[i].location);
+						//printf("equal %f", difftime(mktime(&events[count].time), mktime(&events[i].time)));
+						count ++;
+						break;
 					}
-					else if (difftime(mktime(&events[count].time), mktime(&events[i].time)) <= 0){
-						strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[i].time);
+					else if (difftime(mktime(&events[count].time), mktime(&events[i].time)) < 0|count == 0){
+						strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[count].time);
+						//printf("less than %f", difftime(mktime(&events[count].time), mktime(&events[i].time)));
 						printf("%s,%s\n", time, events[count].location);
+
+						break;
 					}
-					else if (difftime(mktime(&events[count].time), mktime(&events[i].time)) >= 0){
-						strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[i].time);
-						printf("%s,%s\n", time, events[i].location);
+					else if (difftime(mktime(&events[count].time), mktime(&events[i].time)) > 0){
+
+						break;
+						//printf("greater than %f", difftime(mktime(&events[count].time), mktime(&events[i].time)));
+						//strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[i].time);
+						//printf("%s,%s\n", time, events[i].location);
 					}
 
 				}
 				//printf("%s,%i,%s", events[count].date, events[count].time, events[count].location);
 				count ++;
 			}
-			if (strrchr(action, 'X')){
+			else if (strrchr(action, 'X')){
 				for (int i = 0; i <= count; i++){
 				if (strcmp(events[i].title, title) == 0){
 					strcpy(events[i].date, date);
@@ -85,16 +94,41 @@ int main(int argc, char *argv[]){
 					events[count].time.tm_min = min;
 					strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[i].time);
 					printf("%s,%s\n", time, events[i].location);
+
 				}
 				}
+				count ++;
 			}
-			if (strrchr(action, 'D')){
+			else if (strrchr(action, 'D')){
+				strcpy(events[count].title, title);
+				events[count].time.tm_hour = hour;
+				events[count].time.tm_min = min;
+				events[count].time.tm_year = year;
+				events[count].time.tm_mon = month-1;
+				events[count].time.tm_mday = day;
+				events[count].time.tm_isdst = 0;
 				for (int i = 0; i <= count; i++){
-				if (events[i].time.tm_year == year && events[i].time.tm_mon == month && events[i].time.tm_mday == day && events[i].time.tm_hour == hour && events[i].time.tm_min == min){
-					strcpy(events[i].location, "NA");
-					printf("%d/%d/%d,--:--,NA\n",  events[i].time.tm_mon,events[i].time.tm_mday,events[i].time.tm_year);
+					if (difftime(mktime(&events[count].time), mktime(&events[i].time)) == 0 && i != count){
+						strftime(time, SIZE, "%m/%d/%g", &events[count].time);
+						printf("%s,--:--,NA\n",  time);
+						//printf("equal %f", difftime(mktime(&events[count].time), mktime(&events[i].time)));
+						break;
+					}
+					else if (difftime(mktime(&events[count].time), mktime(&events[i].time)) < 0|count == 0){
+						strftime(time, SIZE, "%m/%d/%g", &events[count].time);
+						//printf("less than %f", difftime(mktime(&events[count].time), mktime(&events[i].time)));
+						printf("%s,--:--,NA%i\n",  time,count);
+						break;
+					}
+					else if (difftime(mktime(&events[count].time), mktime(&events[i].time)) > 0){
+						break;
+						//printf("greater than %f", difftime(mktime(&events[count].time), mktime(&events[i].time)));
+						//strftime(time, SIZE, "%m/%d/%g,%H:%M", &events[i].time);
+						//printf("%s,%s\n", time, events[i].location);
+					}
+
 				}
-				}
+				count ++;
 			}
 
 
